7087d23791408ce468550154262b89a0b8d10346
slammokl <shungom@yandex.ru>
2024-04-19 21:46:23 +0300

    version 0.1

--- src/app.py
+++ src/app.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI, Request 
 from fastapi.responses import HTMLResponse 
+from fastapi.staticfiles import StaticFiles 
+from fastapi.templating import Jinja2Templates 
 
 app = FastAPI() 
 
-@app.get("/")
-async def read_item(nm: str = "Recruto", msg: str = "Давай дружить"):
-    return {"name" : nm, "message" : msg}
+templates = Jinja2Templates(directory = 'templates') 
+
+@app.get("/", response_class = HTMLResponse)
+async def read_item(request: Request, nm: str = "Recruto", msg: str = "Давай дружить"):
+    return templates.TemplateResponse(
+        request=request, 
+        name="item.html", 
+        context={"nm": nm, 'msg' : msg}
+    )
