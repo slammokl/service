115de9fd4ef828f0ca2662c20d2fadb708cd8bae
slammokl <shungom@yandex.ru>
2024-04-20 02:18:05 +0300

    version 1.2

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 
 app = FastAPI() 
 
-@app.get("https://service-ten-self.vercel.app//")
+@app.get("/")
 async def show_message(name: str = "World", message: str = "I want to be friend"):
     html_content = """
         <html>
